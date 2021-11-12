c0840ad08259b930e555a26345460198be6fd0ec
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-11-11 18:31:48 -0600

    joined parties table with candidates table and refactored get routes

--- server.js
+++ server.js
@@ -21,39 +21,49 @@ const db = mysql.createConnection(
 );
 
 // Get all candidates
-// app.get('/api/candidates', (req, res) => {
-//   const sql = `SELECT * FROM candidates`;
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT candidates.*, parties.name
+               AS party_name
+               FROM candidates
+               LEFT JOIN parties
+               ON candidates.party_id = parties.id`;
 
 
-//   db.query(sql, (err, rows) => {
-//     if (err) {
-//       res.status(500).json({ error: err.message});
-//       return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: rows
-//     });
-//   });
-// });
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message});
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
 
 
 // Get a single candidate
-// app.get('/api/candidate/:id', (req, res) => {
-//   const sql =  `SELECT * FROM candidates WHERE id = ?`;
-//   const params = [req.params.id];
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT candidates.*, parties.name
+               AS party_name
+               FROM candidates
+               LEFT JOIN parties
+               ON candidates.party_id = parties.id
+               WHERE candidates.id = ?`;
 
-//   db.query(sql, params, (err, row) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message })
-//       return
-//     }
-//     res.json({
-//       message: 'success',
-//       data: row
-//     });
-//   });
-// });
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+      return
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
 
 
 // Delete a candidate
