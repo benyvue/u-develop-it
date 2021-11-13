f3d84c378e3f72160b1f1b549172ee44e2f8e999
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-11-13 17:22:25 -0600

    edit connection info

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      user: 'root',
-      password: 'Bennkoly1#',
+      user: '',
+      password: '',
       database: 'election'
     },
     console.log('Connected to the election database.')
